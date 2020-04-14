a467ef6e5653fc5eb71f39e24e0e59f54ede9191
Neikan <sladkov.e.m@outlook.com>
2020-04-14 15:24:15 +0300

    Работа над страницой каталога

--- source/js/menu.js
+++ source/js/menu.js
@@ -11,9 +11,6 @@ const toggle = document.querySelector(".navigation__toggle");
 
 navigation.classList.remove(Classes.NO_JS);
 
-// Не забыть убрать:
-// navigation.classList.remove(Classes.CLOSED);
-
 const toggleClickHandler = () => {
   if (navigation.classList.contains(Classes.CLOSED)) {
     navigation.classList.remove(Classes.CLOSED);
